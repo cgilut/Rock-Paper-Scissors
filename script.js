774dbfa4f7a3d8287fd7602524f2eb506c078072
cgilut <117078690+cgilut@users.noreply.github.com>
2023-09-26 13:10:10 +0200

    Get playerSelection and computerSelection

--- script.js
+++ script.js
@@ -4,16 +4,22 @@
 let playerSelection = prompt("Rock, paper or scissors?");
     playerSelection = playerSelection.toLowerCase()
         while (
-            playerSelection != "rock" && 
-            playerSelection != "paper"  && 
+            playerSelection != "rock" &&
+            playerSelection != "paper" &&
             playerSelection != "scissors"
         ) {
-            playerSelection = prompt("Rock, paper or scissors?");
+            playerSelection = prompt("Rock, paper or scissors?!");
             playerSelection = playerSelection.toLowerCase()
         }
 alert(`You have chosen ${playerSelection}.`);
 
 
 // get a random input from computer and save it into a function
+
+let signArray = [ "rock" , "paper" , "scissors" ];
+let randomSign = Math.floor(Math.random() * 3);
+let computerSelection = signArray[randomSign];
+alert(computerSelection);
+
 
 // compare two function and decide the winner
