16e88fcf5144da29ac57627fc1106df200a53001
cgilut <117078690+cgilut@users.noreply.github.com>
2023-09-29 12:22:54 +0200

    Create a function to play a round

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict"
 
 // get input from the user and save it into a function
+
 function getPlayerSelection () {
     let playerSign = prompt("Rock, paper or scissors?");
         playerSign = playerSign.toLowerCase()
@@ -13,42 +14,40 @@ function getPlayerSelection () {
                 playerSign = playerSign.toLowerCase()
             }
             return playerSign;
-    // alert(`You have chosen ${playerSign}.`);
 }
 
 
 // get a random input from computer and save it into a function
+
 function getComputerSelection() {
     let signArray = [ "rock" , "paper" , "scissors" ];
     let randomSign = Math.floor(Math.random() * 3);
     let computerSign = signArray[randomSign];
-    // console.log(computerSign);
     return computerSign;
 }
-
-
-// compare two function and decide the winner
 
 let playerSelection = getPlayerSelection()
 let computerSelection = getComputerSelection()
 console.log(playerSelection, computerSelection)
 
-if (
-    (playerSelection == "rock" && computerSelection == "rock") ||
-    (playerSelection == "paper" && computerSelection == "paper") ||
-    (playerSelection == "scissors" && computerSelection == "scissors")
-) {
-    alert("Draw!");
-} else if (
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "scissors" && computerSelection == "paper")
-) {
-    alert("You win!");
-} else if (
-    (playerSelection == "rock" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "rock")
-) {
-    alert("You lose!");
+// compare two function and decide the winner
+
+function playRound() {
+    if (
+        (playerSelection == "rock" && computerSelection == "rock") ||
+        (playerSelection == "paper" && computerSelection == "paper") ||
+        (playerSelection == "scissors" && computerSelection == "scissors")
+    ) {
+        alert("Draw!");
+    } else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")
+    ) {
+        alert("You win!");
+    } else {
+        alert("You lose!");
+    }
 }
+
+playRound()
