6c3e8c3dc0781e2507f77a5f4537d110b2fe3542
cgilut <117078690+cgilut@users.noreply.github.com>
2024-02-08 11:58:48 +0100

    Remove visibility.js file

--- visibility.js
+++
@@ -1,5 +0,0 @@
-// const letsPlayBtn = document.querySelector(".wrapper__lets-play-btn")
-
-// letsPlayBtn.addEventListener("click", () => {
-//     letsPlayBtn.classList.add("display-none");
-// });
