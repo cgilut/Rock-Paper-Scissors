d5bf7488c75c8d092c1712bc16d7d179ae3692ac
cgilut <117078690+cgilut@users.noreply.github.com>
2024-02-18 12:18:44 +0100

    Add a russian version of the website

---
+++ ru-script.js
@@ -0,0 +1,178 @@
+"use strict"
+
+const buttonSigns = document.querySelectorAll('.wrapper__button-sign')
+const yourSignDiv = document.querySelector('.wrapper__your-sign')
+const compSignDiv = document.querySelector('.wrapper__computer-sign')
+const roundOutcomeText = document.querySelector('.wrapper__outcome')
+const yourScore = document.querySelector('.wrapper__your-score')
+const compScore = document.querySelector('.wrapper__computer-score')
+const endOfGamePopup = document.querySelector('.endOfGamePopup')
+const gameOutcomeText = document.querySelector('.endOfGamePopup__inner-text')
+const closeButton = document.querySelector('#endOfGamePopup__closeButton')
+const body = document.querySelector('body')
+const modeButton = document.querySelector('.header__buttons-mode')
+let playerCounter = 0;
+let computerCounter = 0;
+
+function whenButtonClicked() {
+
+    let playerSelection = this.id;
+    let computerSelection = getComputerSelection();
+
+
+    function getComputerSelection() {
+
+        let signArray = [ "камень" , "бумага" , "ножницы" ];
+        // math.random gives a number from 0 to 1, excluding 1
+        // then we multiply it by 3 and round down,
+        // that way we get either 0, 1 or 2, which corresponds
+        // to the objects in array
+        let randomSign = Math.floor(Math.random() * 3);
+        let computerSign = signArray[randomSign];
+        return computerSign;
+        
+    }
+
+    yourSignDiv.classList.remove('won', 'lost');
+    compSignDiv.classList.remove('won', 'lost');
+
+    yourSignDiv.innerHTML = `<img src="assets/${playerSelection}.png" 
+        alt="Твой знак" class = "wrapper__img-${playerSelection}">`;
+    yourSignDiv.classList.add('your-choice');
+
+    compSignDiv.innerHTML = `<img src="assets/${computerSelection}.png" 
+        alt="Знак Компьютер" class = "wrapper__img-${computerSelection}">`;
+    compSignDiv.classList.add('comp-choice')
+
+    function playRound() {
+
+        if (
+            (playerSelection == "камень" && computerSelection == "камень") ||
+            (playerSelection == "бумага" && computerSelection == "бумага") ||
+            (playerSelection == "ножницы" && computerSelection == "ножницы")
+        ) {
+            roundOutcomeText.textContent = 
+                `${playerSelection.charAt(0).toUpperCase()}${playerSelection.slice(1)}
+                = ${computerSelection}, ничья!`;
+        } else if (
+            (playerSelection == "камень" && computerSelection == "ножницы") ||
+            (playerSelection == "бумага" && computerSelection == "камень") ||
+            (playerSelection == "ножницы" && computerSelection == "бумага")
+        ) {
+            roundOutcomeText.textContent = 
+                `${playerSelection.charAt(0).toUpperCase()}${playerSelection.slice(1)}
+                > ${computerSelection}, ты победил в этом раунде!`;
+            yourSignDiv.classList.add('won');
+            compSignDiv.classList.add('lost');
+            playerCounter++;
+            yourScore.textContent = `Ты: ${playerCounter}`;
+        } else {
+            roundOutcomeText.textContent = 
+                `${computerSelection.charAt(0).toUpperCase()}${computerSelection.slice(1)} 
+                > ${playerSelection}, ты проиграл в этом раунде!`;
+            yourSignDiv.classList.add('lost');
+            compSignDiv.classList.add('won');
+            computerCounter++;
+            compScore.textContent = `Компьютер: ${computerCounter}`;
+        }
+
+        if (playerCounter === 3 || computerCounter === 3) {
+            endTheGame();
+        }
+
+    }
+
+    function endTheGame() {
+
+        endOfGamePopup.classList.add('active')
+        body.classList.add('blur')
+        if (playerCounter === 3) {
+            gameOutcomeText.innerHTML = 
+            `<span class="outcome-highlight">Победа!</span><br> 
+            ${playerCounter} : ${computerCounter}`;
+        } else {
+            gameOutcomeText.innerHTML = 
+            `<span class="outcome-highlight">Поражение!</span><br>
+            ${playerCounter} : ${computerCounter}`;
+        }
+
+        closeButton.addEventListener('click', () => {
+            setTimeout(resetToDefaultState, 200);
+            endOfGamePopup.classList.remove('active');
+        });
+        
+    }
+
+    function resetToDefaultState() {
+
+        roundOutcomeText.textContent = 'Выбери знак, игра до 3х побед!';
+        yourSignDiv.innerHTML = '?';
+        compSignDiv.innerHTML = '?';
+        yourScore.textContent = 'Ты: 0';
+        compScore.textContent = 'Компьютер: 0';
+        playerCounter = 0;
+        computerCounter = 0;
+        yourSignDiv.classList.remove('won', 'lost', 'your-choice');
+        compSignDiv.classList.remove('won', 'lost', 'comp-choice');
+        body.classList.remove('blur')
+    }
+
+    playRound();
+}
+
+buttonSigns.forEach(button => {
+
+    button.addEventListener('click', whenButtonClicked);
+
+});
+
+const mode = document.querySelector('.header__buttons-mode')
+const el = document.documentElement.style
+let lightMode = localStorage.getItem('lightMode')
+
+mode.addEventListener('click', () => { 
+
+    lightMode = localStorage.getItem('lightMode');
+    if (lightMode !== 'enabled') {
+        // if its dark mode, switch to light
+        // and change the icon sun
+        localStorage.setItem('lightMode', 'enabled')
+        modeButton.innerHTML = '<ion-icon name="moon-outline"></ion-icon>'
+        lightModeColors()
+    }
+    else {
+        // if its light mode, switch to dark
+        // and change the icon to moon
+        localStorage.setItem('lightMode', null)
+        modeButton.innerHTML = '<ion-icon name="sunny-outline"></ion-icon>'
+        darkModeColors()
+    }
+
+});
+
+if (lightMode === 'enabled') {
+    lightModeColors()
+    modeButton.innerHTML = '<ion-icon name="moon-outline"></ion-icon>'
+};
+
+function lightModeColors() {
+    el.setProperty('--font-color', '#000')
+    el.setProperty('--header-color', '#777777')
+    el.setProperty('--background-color', '#cccccc')
+    el.setProperty('--wrapper-color', '#bbbbbb')
+    el.setProperty('--button-glow', '0 0 10px 5px rgb(38, 0, 255, 0.5)')
+    el.setProperty('--orange', '#000')
+    el.setProperty('--sign-button-color', '#fff')
+    el.setProperty('--sign-button-hover', '#fcffd0')
+};
+
+function darkModeColors() {
+    el.setProperty('--font-color', '#fff')
+    el.setProperty('--header-color', '#000')
+    el.setProperty('--background-color', '#333333')
+    el.setProperty('--wrapper-color', '#414141')
+    el.setProperty('--button-glow', '0 0 10px 5px rgba(255, 255, 255, 0.3)')
+    el.setProperty('--orange', '#f0705a')
+    el.setProperty('--sign-button-color', '#606060')
+    el.setProperty('--sign-button-hover', '#adadad')
+};
